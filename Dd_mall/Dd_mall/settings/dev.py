fb1fe73e87a99415474003bade8b47c44a10b745
朱耀东 <zhuyaodong219@hotmail.com>
2022-02-16 01:52:56 +0800

    配置MYSQL數據庫

--- Dd_mall/Dd_mall/settings/dev.py
+++ Dd_mall/Dd_mall/settings/dev.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'Dd_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '192.168.3.55',
+        'PORT': 3306,
+        'USER': 'DdShop',
+        'PASSWORD': 's910219',
+        'NAME': 'ddshop',
     }
 }
 
